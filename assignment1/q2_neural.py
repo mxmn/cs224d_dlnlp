1c532bd497e178b89a254ea310cb2302fff5083a
Maxim Neumann <neumann.maxim@gmail.com>
2016-05-08 22:27:16 -0700

    neural network basics

--- assignment1/q2_neural.py
+++ assignment1/q2_neural.py
@@ -6,9 +6,9 @@ from q2_sigmoid import sigmoid, sigmoid_grad
 from q2_gradcheck import gradcheck_naive
 
 def forward_backward_prop(data, labels, params, dimensions):
-    """ 
-    Forward and backward propagation for a two-layer sigmoidal network 
-    
+    """
+    Forward and backward propagation for a two-layer sigmoidal network
+
     Compute the forward propagation and for the cross entropy cost,
     and backward propagation for the gradients for all parameters.
     """
@@ -26,22 +26,33 @@ def forward_backward_prop(data, labels, params, dimensions):
     b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
 
     ### YOUR CODE HERE: forward propagation
-    raise NotImplementedError
+    x = data
+    z1 = x.dot(W1) + b1
+    h = sigmoid(z1)
+    y = softmax(h.dot(W2) + b2)
+    cost = -np.sum(labels * np.log(y))
     ### END YOUR CODE
-    
+
     ### YOUR CODE HERE: backward propagation
-    raise NotImplementedError
+    dz2 = y - labels
+    dh1 = dz2.dot(W2.T)
+    dz1 = dh1 * sigmoid_grad(h)
+
+    gradW2 = h.T.dot(dz2)
+    gradb2 = np.sum(dz2, axis=0)
+    gradW1 = x.T.dot(dz1)
+    gradb1 = np.sum(dz1, axis=0)
     ### END YOUR CODE
-    
+
     ### Stack gradients (do not modify)
-    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(), 
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
         gradW2.flatten(), gradb2.flatten()))
-    
+
     return cost, grad
 
 def sanity_check():
     """
-    Set up fake data and parameters for the neural network, and test using 
+    Set up fake data and parameters for the neural network, and test using
     gradcheck.
     """
     print "Running sanity check..."
@@ -52,23 +63,24 @@ def sanity_check():
     labels = np.zeros((N, dimensions[2]))
     for i in xrange(N):
         labels[i,random.randint(0,dimensions[2]-1)] = 1
-    
+
     params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
         dimensions[1] + 1) * dimensions[2], )
 
     gradcheck_naive(lambda params: forward_backward_prop(data, labels, params,
         dimensions), params)
 
-def your_sanity_checks(): 
+
+def your_sanity_checks():
     """
     Use this space add any additional sanity checks by running:
-        python q2_neural.py 
+        python q2_neural.py
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    #raise NotImplementedError
     ### END YOUR CODE
 
 if __name__ == "__main__":
